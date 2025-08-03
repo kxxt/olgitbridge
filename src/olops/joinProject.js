83e601b9b61fab4c8f25131389f817b74f238b12
Levi Zim <rsworktech@outlook.com>
2025-08-03 23:12:51 +0800

    Fix joinProject for overleaf v5

--- src/olops/joinProject.js
+++ src/olops/joinProject.js
@@ -2,43 +2,56 @@
 | Makes the real-time operations to join a project
 | using socket io.
 */
-const io = require( '../../lib/socket.io-client/lib/io' );
+const io = require('../../lib/socket.io-client');
+const { XMLHttpRequest } = require('../XMLHttpRequest')
+
+io.util.request = function () {
+	const xhr = new XMLHttpRequest()
+	return xhr
+}
+
 
 module.exports =
-	async function( client, olServer, project_id )
-{
-	console.log( client.count, 'io connect to', project_id );
-	const cookieJar = client.defaults.jar;
-	const cookie = cookieJar.getCookieStringSync( olServer );
-	const socket = io.connect(
+	async function (client, olServer, project_id) {
+		console.log(client.count, 'io connect to', project_id);
+		const cookieJar = client.defaults.jar;
+		const cookie = cookieJar.getCookieStringSync(olServer);
+		const socket = io.connect(
 			olServer,
 			{
 				withCredentials: true,
 				cookie: cookie,
-				transports: [ 'websocket' ],
+				transports: ['websocket'],
 				'force new connection': true,
+				query: new URLSearchParams({ projectId: project_id }).toString(),
+				// Disable auto connect because we might get joinProjectResponse before
+				// setting up the listener for it with auto connect
+				'auto connect': false
 			}
 		);
-	let project;
-
-	// this is a bad workaround, sometimes socket.io just doesn't seem to reply
-	// (or reply to a previous connection, there are some fixes in newer versions it seems)
-	// after a timeout just try again. Promise logic should discard the respective other event
-	// should it occour.
-	while( !project )
-	{
-		const promise = new Promise( ( resolve, reject ) => {
-			socket.emit(
-				'joinProject',
-				{ 'project_id': project_id },
-				( self, res, owner, number ) => resolve( res )
-			);
-			setTimeout( ( ) => resolve( undefined ), 1000 );
-		} );
-		project = await promise;
-		if( !project ) console.log( client.count, '*** timeout on socket.io, retrying' );
-	}
-	console.log( client.count, 'iosocket disconnect' );
-	socket.disconnect( );
-	return project;
-};
+		let storedProject;
+		socket.on('joinProjectResponse', resp => {
+			const { publicId, project, permissionsLevel, protocolVersion } = resp
+			// console.log(project, protocolVersion)
+			storedProject = project;
+		})
+		socket.on('connectionRejected', err => {
+			storedProject = null
+			console.log(err)
+		})
+		socket.on('disconnect', () => {
+			console.log("Disconnected from socket.io")
+		})
+		socket.socket.connect()
+		// this is a bad workaround, sometimes socket.io just doesn't seem to reply
+		// (or reply to a previous connection, there are some fixes in newer versions it seems)
+		// after a timeout just try again. Promise logic should discard the respective other event
+		// should it occour.
+		while (storedProject === undefined) {
+			await new Promise(r => setTimeout(r, 1000));
+			console.log('iosocket timeout');
+		}
+		console.log(client.count, 'iosocket disconnect');
+		socket.disconnect();
+		return storedProject;
+	};
